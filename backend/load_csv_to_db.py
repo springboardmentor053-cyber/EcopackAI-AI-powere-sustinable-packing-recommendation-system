442b91e4a47c4344952f0a6dcca0ec266abdf11d
Esakki Raja <esakkiraja2528@gmail.com>
2026-02-03 21:28:43 +0530

    Updated app.py

--- backend/load_csv_to_db.py
+++ backend/load_csv_to_db.py
@@ -1,17 +1,31 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-engine = create_engine(
-    "postgresql://postgres:Esakki%402008@localhost:5432/ecopackai"
+# Load environment variables
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
-
+# Load CSV
 df = pd.read_csv(
     r"D:/Project ecopackai/data/materials_module2_final.csv"
 )
 
-df.columns = df.columns.str.strip()
+# Create DB engine
+engine = create_engine(DATABASE_URL)
 
+# Upload to PostgreSQL
 df.to_sql("materials", engine, if_exists="append", index=False)
 
-print("✅ CSV imported into PostgreSQL")
+print("✅ CSV imported into PostgreSQL successfully!")
