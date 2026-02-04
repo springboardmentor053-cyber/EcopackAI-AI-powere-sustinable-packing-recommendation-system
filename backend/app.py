953658ec43d58af3357da273b4f7ca8cc7116dac
VIVEK-MARRI <marrivivek26@gmail.com>
2026-02-04 12:02:11 +0530

    Modified app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import pandas as pd
+from services.recommendation_service import run_recommendation
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +9,16 @@ CORS(app)
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "EcoPackAI backend running"})
+
+
+@app.route("/api/recommend", methods=["POST"])
+def recommend():
+    data = request.json
+    df = pd.DataFrame(data)
+
+    result = run_recommendation(df)
+
+    return jsonify(result.to_dict(orient="records"))
 
 
 if __name__ == "__main__":
